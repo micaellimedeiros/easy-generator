9add411c4b158cc21aa5a87d01598771295225d4
Micaelli Medeiros <m.amedeiros@hotmail.com>
2023-11-28 10:23:30 -0300

    chore: add routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+import LoginForm from "./components/LoginForm";
+import ApplicationPage from "./components/ApplicationPage";
+import RegisterForm from "./components/RegisterForm";
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+      <Route path="/register" element={<RegisterForm />} />
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="/application" element={<ApplicationPage />} />
+    </Router>
+  );
+};
 
-export default App
+export default App;
