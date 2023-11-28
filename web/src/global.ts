998b5be88e0177523125b000cfc60692e4d10272
Micaelli Medeiros <m.amedeiros@hotmail.com>
2023-11-28 20:25:22 -0300

    chore: add global styles

---
+++ web/src/global.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: "Segoe UI", Arial, sans-serif;
+  }
+
+  svg {
+    cursor: pointer;
+  }
+`;
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import App from "./App.tsx";
+
+import AuthLayout from "./components/AuthLayout/index.tsx";
+import GlobalStyle from "./global";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <AuthLayout>
+      <App />
+    </AuthLayout>
+
+    <GlobalStyle />
   </React.StrictMode>
 );
